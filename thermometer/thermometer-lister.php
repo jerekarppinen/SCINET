c4f426fe67bd4ff3f2f723aadc785c04961a4138
Jere Karppinen <jere.karppinen88@gmail.com>
2016-08-28 16:25:26 +0300

    a bit of structure

---
+++ thermometer/thermometer-lister.php
@@ -0,0 +1,67 @@
+﻿<?php
+
+include("../globals.php");
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+if(isset($_GET['lastday'])) {
+	$sql = "SELECT * FROM temperatures WHERE DATETIME >= now() - INTERVAL 1 DAY;";
+
+	$result = $conn->query($sql);
+
+	$temparray = array();
+
+	while($row = $result->fetch_assoc()) {
+
+	$temparray[] = $row;
+
+	}
+
+	echo json_encode($temparray
+}
+
+
+
+
+if(isset($_GET['latest'])) {
+
+	$sql = "SELECT * FROM temperatures ORDER BY DATETIME DESC LIMIT 1;";
+	$result = $conn->query($sql);
+
+	$temparray = array();
+
+	while($row = $result->fetch_assoc()) {
+
+	$temparray[] = $row;
+
+	}
+
+	echo json_encode($temparray);
+	
+}
+else {
+
+	$sql = "SELECT * FROM temperatures ORDER BY DATETIME DESC;";
+	$result = $conn->query($sql);
+
+	$temparray = array();
+
+	while($row = $result->fetch_assoc()) {
+
+	$temparray[] = $row;
+
+	}
+
+	echo json_encode($temparray);
+
+}
+
+$conn->close();
+
+?>
